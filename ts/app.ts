057fb3fe5d2f04f926f174278c01885d8578f6a1
Helis Montes <helismontes@gmail.com>
2021-03-04 14:39:22 -0500

    Initial commit

---
+++ ts/app.ts
@@ -0,0 +1,28 @@
+import {autos} from "./db"
+
+const dataForm:any = {
+    marca: document.querySelector("#marca"),
+    year: document.querySelector("#year"),
+    minimo: document.querySelector("#minimo"),
+    maximo: document.querySelector("#maximo"),
+    puertas: document.querySelector("#puertas"),
+    transmision: document.querySelector("#transmision"),
+    color: document.querySelector("#color"),
+    resultado: document.querySelector("#resultado")
+}; Object.freeze(dataForm);
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    uploadAuto()
+})
+
+const uploadAuto = () => {
+    autos.map(autos =>{
+        const uploadHMTL:any = document.createElement('p')
+        const { marca, modelo, year, precio, puertas, color, transmision } = autos
+        uploadHMTL.textContent = `
+            ${marca} - ${modelo} - ${year} - ${puertas} Puertas - Transmisión ${transmision} - Precio: ${precio} - Color: ${color} 
+        
+        `;
+        dataForm.resultado.appendChild(uploadHMTL);
+    })
+}
